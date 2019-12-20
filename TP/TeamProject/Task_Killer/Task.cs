ed9d0e4f094c4008c12a240374b6b486b4119fa5
DenisValeevR <denis.valeevr@gmail.com>
2019-12-20 15:36:57 +0300

    Add files via upload
    Проект и интерфейс без дизайна

---
+++ TP/TeamProject/Task_Killer/Task.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Task_Killer
+{
+    public class Task
+	{
+        public DateTime DeadLine { get; set; }
+		public string Text{ get; set;}
+		public bool IsDone { get; set; }
+	}
+}
---
+++ TP/TeamProject/Task_Killer/User.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task_Killer
+{
+    public class User
+    {
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public int Age { get; set; }
+		public string Login { get; set; }
+		public string Pass { get; set; }
+        public List<Note> Notes { get; set; } = new List<Note>();
+        public List<Task> Tasks { get; set; } = new List<Task>();
+		public User(string name, string surname, int age, string login, string pass) 
+		{
+			Name=name;
+			Surname=surname;
+			Age=age;
+			Login=login;
+			Pass=pass;
+		}
+
+       
+    }
+}
